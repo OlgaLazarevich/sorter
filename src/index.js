3b1b0f8af56ca95efc77614ebfc60d8dc15e6c9f
helga <helga>
2018-09-11 20:54:44 +0300

    metods of sorter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,79 @@
 class Sorter {
+    
   constructor() {
-    // your implementation
+      
+    this.arr = [];
+      
   }
 
   add(element) {
-    // your implementation
+      
+    this.arr.push(element);
+      
   }
 
   at(index) {
-    // your implementation
+    
+      return  this.arr[index];
   }
 
   get length() {
-    // your implementation
+    
+      return this.arr.length;
+      
   }
 
   toArray() {
-    // your implementation
+    
+      return this.arr;
+      
   }
 
   sort(indices) {
-    // your implementation
+      
+      let cut = [],
+          min = Math.min.apply(null, indices);
+      
+      function bubbleSort(a) {
+
+        for (let i = 0, len = a.length; i < len; i++) {
+
+            for (let i = 1, len = a.length; i < len; i++) {
+
+                let current = i,
+                    prev = i - 1;
+
+                if (a[prev] > a[current]) {
+
+                    let copyPrev = a[prev];
+
+                    a.splice(prev, 2, a[current]);
+
+                    a.splice(current, 0, copyPrev);
+                }
+
+            }
+        }
+
+    }
+      
+      for(let i = 0; i < indices.length; i++){
+          
+          cut.push(this.arr[indices[i]]);
+          
+      }
+      
+       bubbleSort(cut);
+       bubbleSort(indices);
+      
+      
+      for(let i = 0; i < indices.length; i++){
+          
+          this.arr[indices[i]] = cut[i];
+          
+      }
+      
+      
   }
 
   setComparator(compareFunction) {
